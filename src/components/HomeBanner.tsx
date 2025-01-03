6f867762a71b5e0fc1f66cea2a56744b020877ab
Webby Ayush <ayush2210019-d@akgec.ac.in>
2025-01-04 02:09:51 +0530

    resume added

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -35,20 +35,38 @@ export default function HomeBanner() {
     }, []);
 
     return (
-        <div className="relative lg:h-[80vh] md:h-[45vh] h-[50vh] bg-gray-50">
+        <div className="relative lg:h-[80vh] md:h-[55vh] h-[65vh] bg-gray-50">
             {/* Hero Content */}
             <div className="relative z-10">
-                <div className="flex flex-col items-center text-6xl bg-clip-text text-transparent bg-gradient-to-r from-[#ff6f00] to-[#ffe601] font-black lg:text-9xl sm:text-7xl md:text-9xl px-12 py-2 relative">
+                <div className="flex flex-col about_blank items-center text-6xl bg-clip-text text-transparent bg-gradient-to-r from-[#ff6f00] to-[#ffe601] font-black lg:text-9xl sm:text-7xl md:text-9xl px-12 py-2 relative">
                     <h1>AYUSH</h1>
                     <h1>KUSHWAHA</h1>
                 </div>
-                <div className="absolute top-[145%] lg:top-[115%] md:top-[120%] inset-0 flex justify-center items-center">
+                <a href='/assets/resume.pdf' target="_blank" className="absolute top-[140%] cursor-pointer lg:top-[115%] md:top-[120%] inset-0 flex justify-center items-center">
                     <img
                         src="/assets/Profile.png"
                         alt="hero"
-                        className="h-80 lg:h-80 object-contain animate-float"
+                        className="h-72 md:h-80 lg:h-80 object-contain animate-float"
                     />
+                </a>
+                <div className="absolute md:h-32 lg:h-32 md:w-28 lg:w-28 h-20 w-14 -rotate-12 top-[130%] left-[77%] md:top-[120%] lg:top-[95%] md:left-[65%] lg:left-[58%]">
+                    <img src="/assets/arrow.png" alt="" />
                 </div>
+                <div className="absolute font-serif top-[177%] left-[85%] md:top-[166%] lg:top-[142%] md:left-[76%] lg:left-[64%]">
+                    <motion.div
+                        initial={{ y: 0 }}
+                        animate={{ y: [-5, 0, -5] }} // Moves up and down smoothly
+                        transition={{
+                            duration: 1.5,
+                            repeat: Infinity,
+                            ease: "easeInOut",
+                        }}
+                        className="lg:text-lg md:text-lg text-xs font-semibold text-red-500"
+                    >
+                        <a href="/assets/resume.pdf" target="_blank">Hire Me</a>
+                    </motion.div>
+                </div>
+
                 <div className="flex text-xs font-semibold flex-col justify-center items-center relative top-[250px] lg:top-[220px] text-center">
                     <p>PASSIONATE FRONT-END DEVELOPER SKILLED</p>
                     <span>IN BUILDING DYNAMIC WEB APPS</span>
